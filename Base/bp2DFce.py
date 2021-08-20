820ae0728260f3df542d9d57a031b50f2e7f1822
Pascal Welke <welke@uni-bonn.de>
2021-08-20 14:55:29 +0200

    add intersect function to Face class

--- Base/bp2DFce.py
+++ Base/bp2DFce.py
@@ -1,8 +1,7 @@
-
-
+from Base.bp2DPnt import Point
 
 class Face2D:
-    def __init__(self, p, q):
+    def __init__(self, p: Point, q: Point):
         self.p = p  # 1st end point of a rectanlge face, type=Point2D
         self.q = q  # 2nd end point of a rectangle face, type=Point2D
 
@@ -14,7 +13,7 @@ class Face2D:
         return not self.__eq__(other)
 
 
-    def interior_contains_point(self, pnt):
+    def interior_contains_point(self, pnt: Point):
         x, y = pnt.get_coord()
         px, py = self.p.get_coord()
         qx, qy = self.q.get_coord()
@@ -28,6 +27,43 @@ class Face2D:
                 return True
 
         return False
+
+    def _intersect_dim(self, sp: int, sq: int, op: int, oq: int):
+        if sp > sq:
+            tmp = sq
+            sq = sp
+            sp = tmp
+        if op > oq:
+            tmp = oq
+            oq = op
+            op = tmp
+
+        return max(sp, op), min(sq, oq)
+
+    def intersect(self, other):
+        '''Return the face that represents the intersection of two faces. If self and other don't intersect, return None.'''
+        spx, spy = self.p.get_coord()
+        sqx, sqy = self.q.get_coord()
+
+        opx, opy = other.p.get_coord()
+        oqx, oqy = other.q.get_coord()
+
+        lx, hx = self._intersect_dim(spx, sqx, opx, oqx)
+        ly, hy = self._intersect_dim(spy, sqy, opy, oqy)
+
+        if lx > hx or ly > hy:
+            return None
+        else:
+            return Face2D(Point(lx, ly), Point(hx, hy))
+
+
+    def length(self):
+        '''Return the length of the face, in units.'''
+        px, py = self.p.get_coord()
+        qx, qy = self.q.get_coord()
+
+        return abs(px - qx) + abs(py - qy) # rectangles are axis parallel. hence one of the two differences must always be zero
+
 
 
     
